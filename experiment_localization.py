7ab2330f811e42991554d602ef15981c6be88323
NaizhengJ <133194232+ChirsJia@users.noreply.github.com>
2025-01-03 14:46:44 +0800

    Add files via upload

--- experiment_localization.py
+++ experiment_localization.py
@@ -8,6 +8,10 @@ Created on Sat Dec 28 21:58:28 2024
 import numpy as np
 import pandas as pd
 import joblib
+import pandas as pd
+import numpy as np
+import geatpy as ea
+import seaborn as sns
 
 
 '''定义stacking集中输出的函数'''
@@ -19,7 +23,36 @@ def stackingpredict(testset):
     return stackingoutput
 
 
+'''
+Simulation COMSOL
+'''
+stacking_x=joblib.load('stacking_x_simulation.pkl') 
+stacking_y=joblib.load('stacking_y_simulation.pkl')  
+tdoaset=np.array([[-7.26016666666667,-6.80283333333333,-14.14160416666675],
+[-4.92347916666667,-5.44512500000000,-10.5258125000000],
+[-4.759125,-4.501875,-8.1248125],
+[-3.22991666666667,-2.6153750000000,-3.60864583333333],
+[-1.99368750000000,-1.99368750000000,-0.936104166666667],
+[-0.314416666666670,-0.986125000000005,0.771749999999997]])
+            
+pointset=np.array([[20,20],[19,19],[18,18],[17,17],[16,16],[15,15]])
+temp1=tdoaset[:,0].copy()
+temp2=tdoaset[:,1].copy()
 
+tdoaset[:,0]=temp2
+tdoaset[:,1]=temp1
+
+import time
+for i in range(6):
+    time_start=time.time()
+    stackingoutput=stackingpredict(np.array(tdoaset[i,:]).reshape(1,-1))
+    time_end=time.time()
+    rmse_eachpoint=np.array(np.sqrt(np.sum((stackingoutput-pointset[i,:])**2,axis=1)))
+    print(stackingoutput)
+    print(rmse_eachpoint)
+    print(time_end-time_start)
+del stacking_y
+del stacking_x
 '''
 experiment
 '''
